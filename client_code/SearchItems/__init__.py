cf7f14514f5ffba44633f4521339ac059391ef1c
samuelpmccord@gmail.com <samuelpmccord@gmail.com>
2026-03-29 13:40:04 +0000

    Edited AdminPanel, Main and SearchItems

--- client_code/SearchItems/__init__.py
+++ client_code/SearchItems/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import SearchItemsTemplate
 from anvil import *
+import plotly.graph_objects as go
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
 import anvil.users
